3fd097f65ce8538034778f10b4775ac81e7fb0c7
Eric <londoed@comcast.net>
2019-09-20 14:34:23 -0400

    Updated for Sonnet 2 and TF2

--- sonnet_nalu.py
+++ sonnet_nalu.py
@@ -5,7 +5,7 @@
 import tensorflow as tf
 import sonnet as snt
 
-class NAC(snt.AbstractModule):
+class NAC(snt.Module):
     """Neural accumulator cell.
 
     The implementation is based on https://arxiv.org/abs/1808.00508.
@@ -15,15 +15,18 @@ class NAC(snt.AbstractModule):
         num_outputs: Integer describing the number of outputs.
 
     """
-    def __init__(self, input_shape, num_outputs, name='nac'):
+    def __init__(self, input_shape, num_outputs, name=None):
         super(NAC, self).__init__(name=name)
         self.num_outputs = num_outputs
 
+    @snt.once
+    def _initialize(self, input_shape):
         shape = [int(input_shape[-1]), self.num_outputs]
         self.W_hat = tf.Variable("W_hat", shape=shape, initializer=tf.initialziers.GlorotUniform())
         self.M_hat = tf.Variable("M_hat", shape=shape, initializer=tf.initializers.GlorotUniform())
 
-    def _build(self, x):
+    def __call__(self, x):
+        self._initialize(x)
         W = tf.nn.tanh(self.W_hat) * tf.nn.sigmoid(self.M_hat)
         return tf.matmul(x, tf.cast(W, 'float64'))
 
@@ -43,10 +46,12 @@ class NALU(snt.AbstractModule):
         self.nac = NAC(input_shape, self.num_outputs)
         self.eps = 1e-7
 
+    @snt.once
+    def _initialize(self, input_shape):
         shape = [int(input_shape[-1]), self.num_outputs]
         self.G = tf.Variable("G", shape=shape, initializer=tf.initializers.GlorotUniform())
 
-    def _build(self, x):
+    def __call__(self, x):
         g = tf.nn.sigmoid(tf.matmul(x, self.G))
         y1 = g * self.nac(x)
         y2 = (1 - g) * tf.exp(self.nac(tf.log(tf.abs(x) + self.eps)))
